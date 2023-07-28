e24f555fc5c8724b328a3880620abb847ad37939
Shiva Shrestha <shivashrestha927@gmail.com>
2023-07-29 00:07:56 +0545

    timer circle added

--- src/Components/MainBody.jsx
+++ src/Components/MainBody.jsx
@@ -1,10 +1,19 @@
 import React from "react";
+import { CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
 
 function MainBody() {
   return (
     <div className="w-full mx-auto">
-      <div className="max-w-[1000px] w-full text-[#fefefe]">
-        <p>POMODORO</p>
+      <div className="max-w-[1000px] w-full">
+        <div>
+          <ul>
+            <li></li>
+          </ul>
+        </div>
+        <div style={{ margin: "auto", width: 500, height: 500 }}>
+          <CircularProgressbar value={66} text="66" />
+        </div>
       </div>
     </div>
   );
