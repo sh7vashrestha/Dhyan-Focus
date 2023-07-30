fede0d1ce0286065d0b949a8b830ecf50082e387
Shiva Shrestha <shivashrestha927@gmail.com>
2023-07-31 01:16:21 +0545

    timer

--- src/Components/timer.jsx
+++ src/Components/timer.jsx
@@ -1,39 +1,45 @@
-import React from "react";
-import { CircularProgressbar } from "react-circular-progressbar";
+import React, { useRef } from "react";
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
+import optionStore from "../store/option";
 
 function MainBody() {
+  const store = optionStore();
+  const secondsLeftRef = useRef(store.secondsLeft);
+  const isPausedRef = useRef(store.isPaused);
+  const modeRef = useRef(store.mode);
+  const totalSeconds = store.timer * 60;
+  const percentage = Math.round(100);
   return (
     <div className="w-full mx-auto text-[#fefefe]">
       <div className="max-w-[1000px] m-auto w-full">
+        <div className="w-auto max-w-[400px] mx-auto mt-2">
+          <CircularProgressbar
+            value={40}
+            strokeWidth={12}
+            text="50"
+            styles={buildStyles({
+              // Rotation of path and trail, in number of turns (0-1)
+              rotation: 1,
 
-        <div
-          style={{
-            margin: "auto",
-            maxWidth: 400,
-            width: "auto",
-            height: "auto", 
-            // Rotation of path and trail, in number of turns (0-1)
-            rotation: 0.25,
+              // Whether to use rounded or flat corners on the ends - can use 'butt' or 'round'
+              strokeLinecap: "butt",
 
-            // Whether to use rounded or flat corners on the ends - can use 'butt' or 'round'
-            strokeLinecap: "butt",
+              // Text size
+              textSize: "20px",
 
-            // Text size
-            textSize: "16px",
+              // How long animation takes to go from one percentage to another, in seconds
+              pathTransitionDuration: 1,
 
-            // How long animation takes to go from one percentage to another, in seconds
-            pathTransitionDuration: 0.5,
+              // Can specify path transition in more detail, or remove it entirely
+              // pathTransition: 'none',
 
-            // Can specify path transition in more detail, or remove it entirely
-            // pathTransition: 'none',
-
-            // Colors
-            pathColor: `rgba(62, 152, 199, ${50 / 100})`,
-            textColor: "#f88",
-            trailColor: "#d6d6d6"
-          }}>
-          <CircularProgressbar value={50} text="50" />
+              // Colors
+              pathColor: store.mode === "focus"? "#5f11c7":"#1b6e1b",
+              textColor: "#fefefe",
+              trailColor: 'rgba(255,255,255,.5)',
+            })}
+          />
         </div>
       </div>
     </div>
--- src/store/option.js
+++ src/store/option.js
@@ -1,10 +1,55 @@
 import { create } from "zustand";
 
 const optionStore = create((set) => ({
-   option:"focus",
-   handleOnClick: (item) => {
-      set({option: item.target.value})
-   },
- }))
+  mode: "focus",
+  timer: 25,
+  repeatFocus: 0,
+  isPaused: true,
+  time: {
+    focus: 25,
+    lbreak: 15,
+    sbreak: 5,
+  },
+  secondsLeft: 0,
+  handleOnClick: (item) => {
+    set({
+      mode: item.target.value,
+      isPaused: true,
+      timer: optionStore.getState().time[item.target.value],
+      repeatFocus: 0,
+    });
+  },
+  switchMode: () => {
+    if (
+      optionStore.getState().mode === "focus" ||
+      optionStore.getState().repeatFocus < 3
+    ) {
+      set({
+        mode: "sbreak",
+        timer: optionStore.getState().time["sbreak"],
+        repeatFocus: optionStore.getState().repeatFocus + 1,
+      });
+    } else if (
+      optionStore.getState().mode === "focus" ||
+      optionStore.getState().repeatFocus >= 3
+    ) {
+      set({
+        mode: "lbreak",
+        timer: optionStore.getState().time["lbreak"],
+        repeatFocus: 0,
+      });
+    } else {
+      set({ mode: "focus", timer: optionStore.getState().time["focus"] });
+    }
+    set({
+      secondsLeft: optionStore.getState().timer * 60,
+    });
+  },
+  initTimer: () => {
+    set({
+      secondsLeft: optionStore.getState().timer * 60,
+    });
+  },
+}));
 
 export default optionStore;
